ab51b39264d1c1743bf869ac6bdec9783182fe18
GhostersH <97006291+GhostersH@users.noreply.github.com>
2023-05-13 22:02:10 -0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -46,7 +46,7 @@ export class HomeComponent implements OnInit {
     this.emisorNombre = emisorData.nombre;
     this.emisorRuc = emisorData.ruc;
 
-    this.http.get<any[]>('api/ControladorAPI/api/v1/centrocostos').subscribe(
+    this.http.get<any[]>('https://aspnetback.azurewebsites.net/api/ControladorAPI/api/v1/centrocostos').subscribe(
       data => {
         this.centroCostos = data;
         console.log(this.centroCostos)
@@ -113,7 +113,7 @@ export class HomeComponent implements OnInit {
           const usuario = (document.getElementById('swal-input-usuario') as HTMLInputElement).value;
           const password = (document.getElementById('swal-input-password') as HTMLInputElement).value;
     
-          const endpoint = '/api/ControladorAPI/loginAutorizador';
+          const endpoint = 'https://aspnetback.azurewebsites.net/api/ControladorAPI/loginAutorizador';
           const url = new URL(endpoint, window.location.origin);
     
           url.searchParams.append('usuario', usuario);
@@ -213,7 +213,7 @@ export class HomeComponent implements OnInit {
   
   guardarNuevoCentroCostos(codigo: string, descripcion: string) {
     if (codigo && descripcion) {
-      const url = `api/ControladorAPI/CentroCostosInsert?codigoCentroCostos=${codigo}&descripcionCentroCostos=${descripcion}`;
+      const url = `https://aspnetback.azurewebsites.net/api/ControladorAPI/CentroCostosInsert?codigoCentroCostos=${codigo}&descripcionCentroCostos=${descripcion}`;
       this.http.get(url).subscribe(
         (response) => {
           console.log(response);
