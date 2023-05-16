69c5a2b46ee097afca7205a3bd6dc6346ca0e4da
gteran <gteran@UIOTIC026>
2023-05-15 20:42:13 -0500

    auth 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -58,13 +58,14 @@ export class LoginComponent  implements OnInit{
   
   
    ngOnInit() {  
-    if(localStorage.getItem('token')) {  
-      this.router.navigate(['/home']);  
-    }  
+   
     this.http.get<any>('https://aspnetback.azurewebsites.net/api/ControladorAPI/api/v1/emisores')  
       .subscribe((data: any[]) => {  
         this.emisores = data.map(emisor => emisor.NombreEmisor);  
       });    
+      if(localStorage.getItem('token')) {  
+        this.router.navigate(['/home']);  
+      }  
   } 
     
   onChangeEmisor(event: Event) {  
